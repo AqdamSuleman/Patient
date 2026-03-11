d3678428d1e615daf6965fb3ea3d0e7b6f0e62ac
Suleman Aqdam <mohd.2428cseai1990@kiet.edu>
2026-03-12 03:51:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -136,5 +136,8 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
